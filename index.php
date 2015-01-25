432f48d49c1d181d3c02138ac4b6d948a833cb8a
Jan Horava <czechinthemail@gmail.com>
2015-01-25 13:39:40 -0800

    renamed project to IP.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <meta charset=utf-8>
 <meta content="width=device-width,initial-scale=1" name=viewport>
-<title>GetIP.php</title>
-<link href=css/getip.css rel=stylesheet>
+<title>IP.php</title>
+<link href=css/ip.css rel=stylesheet>
 
 <?php echo $_SERVER['REMOTE_ADDR']; ?>
